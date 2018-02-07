3f8212c3c8c3bc5313c2cdf2f3ec18154acee389
Leon Xu <heshang0827@126.com>
2018-02-07 10:21:16 +0800

    modify the import path to relative path for can go build and run in anywhere

--- chapter2/sample/main.go
+++ chapter2/sample/main.go
@@ -4,8 +4,15 @@ import (
 	"log"
 	"os"
 
-	_ "github.com/goinaction/code/chapter2/sample/matchers"
-	"github.com/goinaction/code/chapter2/sample/search"
+	//这个改为相对路径 为了可以直接编译运行
+	//This is the relative path
+	//for can go build and run
+	_ "./matchers"
+	"./search"
+	//这个是原始代码 需要目录
+	//This is the origin source
+	//_ "github.com/goinaction/code/chapter2/sample/matchers"
+	//"github.com/goinaction/code/chapter2/sample/search"
 )
 
 // init is called prior to main.
--- chapter2/sample/matchers/rss.go
+++ chapter2/sample/matchers/rss.go
@@ -8,7 +8,13 @@ import (
 	"net/http"
 	"regexp"
 
-	"github.com/goinaction/code/chapter2/sample/search"
+	//这个改为相对路径 为了可以直接编译运行
+	//This is the relative path
+	//for can go build and run
+	"../search"
+	//这个是原始代码 需要目录
+	//This is the origin source
+	//"github.com/goinaction/code/chapter2/sample/search"
 )
 
 type (
